b2d143ac547bb282695d23c6b274fa8f40e87074
Alan <alanlinchao94@163.com>
2022-04-18 13:48:58 +0800

    remove: extra testing code in UIUtils.py

--- utils/UIUtils.py
+++ utils/UIUtils.py
@@ -15,7 +15,6 @@ def ShowMessageDialog(msgType):
         messageBox.exec_()
     elif msgType == MESSAGE_TYPE_ADB_ERROR_QUIT:
         messageBox = QMessageBox(QMessageBox.Warning, "警告", "未找到有效的adb路径。请先将adb配置到PATH环境变量中。")
-        QMessageBox.warning()
         messageBox.addButton(u"关闭", QMessageBox.YesRole)
         messageBox.exec_()
     elif msgType == MESSAGE_TYPE_PARSER_EMPTY_INPUT:
